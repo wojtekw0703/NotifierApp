98631186f527e78046677fe1f393739024588f41
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-10-16 22:45:32 +0200

    Update of files to repo

---
+++ README.md
@@ -0,0 +1,22 @@
+## NotifierApp :memo:
+
+## What is NotifierApp ?
+NotifierApp is a simple script which focuses (by default) on **h3 headers** and checks the changes.
+
+## How does it work ?
+NA downloads the "first version" of website and the runs every 10 seconds to looking for changes. If something changed, it would displays a Windows notification with a message
+
+## Requirements
+[![Generic badge](https://img.shields.io/badge/Python-3.x-green.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/AutoHotKey-x.x-green.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/Module-win10toast-green.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/Module-googletrans-green.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/Module-pyperclip-green.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/Module-time-green.svg)](https://shields.io/)
+
+## Attention !
+This version of code works only on **WINDOWS 10** on account of using **win10toast** module. 
+
+## Screen
+![Notification](https://user-images.githubusercontent.com/33324211/95664461-5239c280-0b48-11eb-9dc9-4994fcc83ae1.PNG)
+
--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-from win10toast import *
+import win10toast
 import time
 
+
 my_bool = True
-my_list = []
-page = requests.get("http://www.wojciechwydmuch.com/blog.php")
-soup = BeautifulSoup(page.content, 'html.parser')
-heading = soup.find_all("h3")
 
-for k in heading:
-    my_list.append(k.text)
+page_to_search = requests.get("http://www.wojciechwydmuch.com/blog.php")
+soup = BeautifulSoup(page_to_search.content, 'html.parser')
+heading_h3 = soup.find_all("h3")
 
 
-while my_bool == True:
-    newer = []
-    page = requests.get("http://www.wojciechwydmuch.com/blog.php")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    heading = soup.find_all("h3")
+list_of_h3 = [k.text for k in heading_h3]
 
-    for k in heading:
-        newer.append(k.text)
 
-    if newer != my_list:
-        toaster = ToastNotifier()
+while my_bool:
+    page_to_search = requests.get("http://www.wojciechwydmuch.com/blog.php")
+    soup = BeautifulSoup(page_to_search.content, 'html.parser')
+    heading_h3 = soup.find_all("h3")
+
+    newer_list = [k.text for k in heading_h3]
+
+
+    if newer_list != list_of_h3:
+        toaster = win10toast.ToastNotifier()
         toaster.show_toast("New notification from NotifierApp", "Python caught changes on website", duration=10)
-        my_list = newer
-    my_list = newer
+        heading_h3 = newer_list
+    heading_h3 = newer_list
     time.sleep(10)
 
 
